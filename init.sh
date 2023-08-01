0e1a2b3f089bdeb01ba09e7097dcd30a924a6701
Katic <karthiksingh5699@gmail.com>
2023-08-01 11:39:28 +0530

    added a new project

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 read -p "Enter file name: " filename
-filepath="src/content/blog/$filename"
+blog_filepath="src/content/blog/$filename"
+links_filepath="src/content/links/$filename"
 
 content=$(cat <<EOF
 ---
@@ -24,6 +25,27 @@ export const components = {
 EOF
 )
 
-echo "$content" > "$filepath"
+links_content=$(cat <<EOF
+---
+title: ""
+description: ""
+---
 
-echo "File created successfully at $filepath"
+import Link from "../../components/Link.vue"
+import Code from "../../components/Code.vue"
+import { createHeading } from "../../components/Heading"
+
+export const components = {
+  a: Link,
+  code: Code,
+  h1: createHeading("h1"),
+  h2: createHeading("h2"),
+  h3: createHeading("h3"),
+}
+EOF
+)
+
+echo "$content" > "$blog_filepath"
+echo "$links_content" > "$links_filepath"
+
+echo "Files created successfully at $blog_filepath and $links_filepath"
